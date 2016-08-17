23d7b6e736546d59fdb1d1af364189af3cb4fabf
Dominique Bashizi <dominique@blupods.com>
2016-08-17 11:53:30 -0400

    Changes from class

--- com/tiy/hospital/Nurse.java
+++ com/tiy/hospital/Nurse.java
@@ -3,6 +3,11 @@ package com.tiy.hospital;
 /**
  * Created by dbashizi on 8/16/16.
  */
-public interface Nurse {
-    public void administerMedicine();
+public abstract class Nurse extends Person {
+
+    public Nurse() {
+        super();
+    }
+
+    public abstract void administerMedicine();
 }
